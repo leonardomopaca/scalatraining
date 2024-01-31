236f8d4093682dd4171e9137f7f19f62422ad52a
Leonardo Mopaca <leonardomopaca@gmail.com>
2024-01-30 19:43:13 -0600

    Training

--- src/main/scala/Complex.scala
+++ src/main/scala/Complex.scala
@@ -1,5 +1,21 @@
 package teste
 
-class Complex {
+class Complex (real: Double, imaginary: Double):
+  def re() = real
+  def im() = imaginary
 
-}
+
+def CallComplex: Unit =
+  new Complex(1.5, 2.3)
+
+class Complex2 (real: Double, imaginary: Double):
+  def re = real
+  def im = imaginary
+
+  override def toString() =
+    "" + re + (if im >= 0 then " + " else "") + im + "i"
+
+@main def CallComplex2: Unit =
+  new Complex2(1.5, 2.3)
+  val c = Complex2(1.5, 2.3)
+  println("Overridden toString(): " + c.toString)
